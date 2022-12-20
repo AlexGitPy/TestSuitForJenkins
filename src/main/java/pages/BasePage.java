9a46805fef31455c9da74dc3d9e42ad337075caf
AlexGitPy <bobkovak96@gmail.com>
2022-12-20 12:21:16 +0300

    add new prop

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,6 +1,8 @@
 package pages;
 
+import com.codeborne.selenide.Configuration;
 import config.RunnerConfig;
+import org.graalvm.compiler.options.Option;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
@@ -12,6 +14,9 @@ public class BasePage {
     @BeforeClass
     @Parameters({"browser", "browserVersion"})
     public void setUp(@Optional("Chrome") String browser, @Optional String browserVersion) {
+        Configuration.headless = true;
+        System.setProperty("chromeoptions.args",
+                "\"--no-sandbox\",\"--disable-dev-shm-usage\"");
         config.setUpConfig(browser, browserVersion);
     }
 }
